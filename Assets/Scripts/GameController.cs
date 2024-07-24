aefa2ae025ac376c4d164ca9aa5619d98f48ab57
Robert Hoff <robbyhoff95@gmail.com>
2024-07-24 16:55:03 -0400

    Implements vertical flow

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -56,6 +56,12 @@ public class GameController : MonoBehaviour
     public Color waterColor = Color.cyan;
     public Color emptyColor = Color.clear;
 
+    [Header("Water Properties")] [Range(0f, 2f)]
+    public float maxWater = 1f;
+
+    [Range(0f, 1f)] public float maxCompression = 0.02f;
+    [Range(0f, 1f)] public float minWater = 0.0001f;
+
     private List<List<SpriteRenderer>> _gridSpriteRenderers;
     private List<List<bool>> _gridWalls;
     private List<List<float>> _gridWater;
@@ -100,10 +106,14 @@ public class GameController : MonoBehaviour
                 }
             }
         }
+
+        // UpdateWater();
     }
 
     private void Update()
     {
+        #region Update Water Sprites
+
         for (var r = 0; r < _gridWater.Count; r++)
         {
             for (var c = 0; c < _gridWater[r].Count; c++)
@@ -122,8 +132,105 @@ public class GameController : MonoBehaviour
                     Color.Lerp(emptyColor, waterColor, waterValue);
             }
         }
+
+        #endregion
     }
 
+    public void UpdateWater()
+    {
+        var new_mass = new List<List<float>>();
+
+        #region Initialize Next Grid
+
+        for (var y = 0; y < _gridWater.Count; y++)
+        {
+            new_mass.Add(new List<float>());
+
+            for (var x = 0; x < _gridWater[y].Count; x++)
+            {
+                new_mass[y].Add(_gridWater[y][x]);
+            }
+        }
+
+        #endregion
+
+        // Take the mass of the current cell and the cell below it and figure out how much water the bottom cell should
+        // contain. If it has less than that, remove the corresponding amount from the current cell and add it to the
+        // bottom cell.
+
+        // Check the cell to the left of this one. If it has less water, move over enough water to make both cells
+        // contain the same amount.
+
+        // Do the same thing for the right neighbour.
+
+        // Do the same thing as in step 1., but for the cell above the current one.
+
+        for (var y = 1; y < _gridWater.Count; y++)
+        {
+            for (var x = 1; x < _gridWater[y].Count; x++)
+            {
+                var isWall = _gridWalls[y][x];
+
+                if (isWall)
+                {
+                    continue;
+                }
+
+                var flow = 0f;
+                var minFlow = 1f;
+                var maxSpeed = 1f;
+                var remaining_mass = _gridWater[y][x];
+
+                if (remaining_mass <= 0f)
+                {
+                    continue;
+                }
+
+                if (!_gridWalls[y -1][x])
+                {
+                    flow = get_stable_state_b(remaining_mass + _gridWater[y - 1][x]) - _gridWater[y - 1][x];
+
+                    if (flow > minFlow)
+                    {
+                        flow *= 0.5f;
+                    }
+
+                    flow = Mathf.Clamp(flow, 0, Mathf.Min(maxSpeed, remaining_mass));
+
+                    new_mass[y][x] -= flow;
+                    new_mass[y - 1][x] += flow;
+
+                    remaining_mass -= flow;
+                }
+
+
+                if (remaining_mass <= 0f)
+                {
+                    continue;
+                }
+            }
+        }
+        
+        _gridWater = new_mass;
+    }
+
+
+    //Returns the amount of water that should be in the bottom cell.
+    private float get_stable_state_b(float totalMass)
+    {
+        if (totalMass <= 1)
+        {
+            return 1;
+        }
+        else if (totalMass < 2 * maxWater + maxCompression)
+        {
+            return (maxWater * maxWater + totalMass * maxCompression) / (maxWater + maxCompression);
+        }
+        else
+        {
+            return (totalMass + maxCompression) / 2;
+        }
+    }
 
     public void OnDrawGizmos()
     {
